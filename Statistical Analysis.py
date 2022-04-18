34a415b1c94cfa5142c4715eef422fc1e609ab99
ThomasBonham <103858876+ThomasBonham@users.noreply.github.com>
2022-04-17 21:30:19 -0600

    Update Statistical Analysis.py

--- Statistical Analysis.py
+++ Statistical Analysis.py
@@ -5,12 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os import path
 
+# setting up the current folder to find the json file
 current_folder = path.dirname(__file__)
 data_test_1_loc = path.join(current_folder, "data test 1.json")
 with open(data_test_1_loc) as f:
     data = json.load(f)
 f.close()
 
+# this just sorts the data for ease of use
 data = np.sort(data)
 # A custom function to calculate
 # probability distribution function
@@ -38,9 +40,12 @@ plt.scatter(x, y, marker='o', s=25, color='red')
 plt.show()
 
 print(data)
+# could've used the builtin mean function, but I did this for some reason
 mean = 0
 for i in data:
     mean += i / len(data)
+
+# displays data
 print(f"Mean: {mean}")
 print(f"Median: {statistics.median(data)}")
 print(f"Mode: {statistics.mode(data)}")
